05e82fe4309e54358c9dc59d5156ab2ad25e8055
Kevin W Matthews <KevinWMatthews@gmail.com>
2015-05-02 16:19:26 -0700

    Refactor

--- MockIO/src/Flash.c
+++ MockIO/src/Flash.c
@@ -1,6 +1,20 @@
 #include "Flash.h"
 #include "m28w160ect.h"
 
+static int writeError(int status)
+{
+  IO_Write(CommandRegister, Reset);
+
+  if (status & VppErrorBit)
+    return FLASH_VPP_ERROR;
+  else if (status & ProgramErrorBit)
+    return FLASH_PROGRAM_ERROR;
+  else if (status & BlockProtectionErrorBit)
+    return FLASH_PROTECTED_BLOCK_ERROR;
+  else
+    return FLASH_UNKNOWN_PROGRAM_ERROR;
+}
+
 int Flash_Write(ioAddress address, ioData data)
 {
   ioData status = 0;
@@ -14,15 +28,7 @@ int Flash_Write(ioAddress address, ioData data)
   }
   if (status != ReadyBit)
   {
-    IO_Write(CommandRegister, Reset);
-    if (status & VppErrorBit)
-      return FLASH_VPP_ERROR;
-    else if (status & ProgramErrorBit)
-      return FLASH_PROGRAM_ERROR;
-    else if (status & BlockProtectionErrorBit)
-      return FLASH_PROTECTED_BLOCK_ERROR;
-    else
-      return FLASH_UNKNOWN_PROGRAM_ERROR;
+    return writeError(status);
   }
 
   if (data != IO_Read(address))
